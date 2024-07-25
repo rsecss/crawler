418b4c0fd8aea51b7be5a86f96047735e2097a00
Zhang Jinhao <132198239+rsecss@users.noreply.github.com>
2024-07-25 12:39:34 +0800

    update "scape_douban.py"

--- scrape_douban.py
+++ scrape_douban.py
@@ -1,30 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
+#伪装请求头
 header = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'
 }
 
-#计数器
+# 计数器
 count = 1
-# 打开一个文件，准备写入数据
-with open('douban_top250_movies.txt', 'w', encoding='utf-8') as file:
-    for stat_num in range(0,250,25):
-        url = f"https://movie.douban.com/top250?start={stat_num}"
+
+#打开文件准备写入
+with open ('douban_top250_test.txt','w',encoding='utf=8') as f:
+    #遍历指定页面
+    for start_number in range (0,250,25):
+        url = f'https://movie.douban.com/top250?start={start_number}'
+
+        #发送请求并获取响应
         response = requests.get(url,headers=header)
-        html = response.text
-        soup = BeautifulSoup(html,"html.parser")
-        # all_titiles = soup.find_all("span",attrs={"class":"title"})
-        # for title in all_titiles:
-        #     title_string=title.string
-        #     if "/" not in title_string:
-        #         print(title_string)
-        all_titiles = soup.find_all("div",class_="hd")
-        for title in all_titiles:
-            movie_title = title.a.span.text
-            print(f"{count}. {movie_title}")
-            # 写入文件
-            file.write(f"{count}. {movie_title}\n")
-            count += 1
+        
+        if response.status_code == 200:
+            html = response.text
+            
+            # 使用 BeautifulSoup 解析页面
+            soup = BeautifulSoup(html,'html.parser')
+            
+            # 寻找标题并提取
+            all_title = soup.find_all('div', class_='hd')
+            for title in all_title:
+                movie_title = title.a.span.text
+                print(f'{count}. {movie_title}\n') 
+                
+                # 写入文件
+                f.write(f'{movie_title}\n')
+                count += 1
+                
 # 输出提示信息
 print("抓取数据并保存到 douban_top250_movies.txt 文件完成。")
